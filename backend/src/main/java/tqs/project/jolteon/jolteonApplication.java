c541ae83c27802f41ecf57f3ddbaa695ce1ccb11
Hbmo1 <henrique.bmo@ua.pt>
2025-06-05 00:07:24 +0100

    Integrate Loki and promtail for logging, viewed in grafana

--- backend/src/main/java/tqs/project/jolteon/jolteonApplication.java
+++ backend/src/main/java/tqs/project/jolteon/jolteonApplication.java
@@ -2,12 +2,17 @@ package tqs.project.jolteon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class jolteonApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(jolteonApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(jolteonApplication.class, args);
+		logger.info("🚀 Jolteon application started successfully!");
 	}
 
 }
